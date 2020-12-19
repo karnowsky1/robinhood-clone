263cee1495f78be0eaae9ae02be10ea8e0588e38
karnowsky1 <kestime9015@bths.edu>
2020-12-19 18:04:57 -0500

    added logo to header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,19 @@
 import React from 'react'
+import Logo from './robinhood.svg'
 
 function Header() {
     return (
         <div className="header__wrapper">
             {/* logo */}
-            <div className="header__logo">
-                Test
+            <div className="header__logo"> 
+                <img src={Logo} width={25}/>
             </div>
             {/* Searchbar */}
+            <div className="header__search">
+              <div className="header__searchcontainer">
+                
+              </div>
+            </div>
             {/* menu items */}
         </div>
     )
