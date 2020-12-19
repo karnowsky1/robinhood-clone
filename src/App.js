cdfa92462e9cc9bd0ee3a401ad895def76b71349
karnowsky1 <kestime9015@bths.edu>
2020-12-19 15:23:22 -0500

    initialized header creation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      
+      {/* Header */}
+      <div className="app__header">
+        <Header />
+      </div>
+      <div className="app__body">
+
+      </div>
+      {/* Body */}
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div className="header__wrapper">
+            {/* logo */}
+            <div className="header__logo">
+                Test
+            </div>
+            {/* Searchbar */}
+            {/* menu items */}
+        </div>
+    )
+}
+
+export default Header
