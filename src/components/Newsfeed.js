3221fc02fdd673511e4868095f6c26d4f86c472f
karnowsky1 <kestime9015@bths.edu>
2020-12-20 17:34:27 -0500

    cdss changes to newseed and and stats component

--- src/components/Newsfeed.js
+++ src/components/Newsfeed.js
@@ -5,7 +5,12 @@ function Newsfeed() {
   return (
     <div className="newsfeed">
       <div className="newsfeed__container">
-
+        <div className="newsfeed__chartSection">
+          <div className="newsfeed__portfolio">
+            <h1>$114,656.84</h1>
+            <p>+$44.63 (+0.04%) Today</p>
+          </div>
+        </div>
       </div>
     </div>
   )
